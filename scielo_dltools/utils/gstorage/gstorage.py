1c7dc7f4c6b740139677a7e93465d71ba316eece
Rafael JPD <rafael.pezzuto@gmail.com>
2021-10-04 17:07:00 -0300

    adiciona método para dicionar conteúdo de bucket

--- scielo_dltools/utils/gstorage/gstorage.py
+++ scielo_dltools/utils/gstorage/gstorage.py
@@ -8,3 +8,11 @@ from google.api_core.exceptions import NotFound
 def _connect(bucket_name):
     storage_client = storage.Client()
     return storage_client.bucket(bucket_name)
+
+
+def list_content(bucket_name):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+
+    for b in bucket.list_blobs():
+        print(b.name)
