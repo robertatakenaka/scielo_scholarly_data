ce91c58dc81d728bc8f57d35a8e580773ff9b2e8
Rafael JPD <rafael.pezzuto@gmail.com>
2021-10-04 20:26:57 -0300

    adiciona métodos para gerar ids de dataset e tabela

--- scielo_dltools/utils/gbq.py
+++ scielo_dltools/utils/gbq.py
@@ -8,3 +8,11 @@ def _connect():
     return bigquery.Client()
 
 
+def _generate_dataset_id(project_name, dataset_name):
+    return '{}.{}'.format(project_name, dataset_name)
+
+
+def _generate_table_id(project_name, dataset_name, table_id):
+    return '{}.{}.{}'.format(project_name, dataset_name, table_id)
+
+
