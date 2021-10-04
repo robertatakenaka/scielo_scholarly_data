58b7ddbc2d3424a545500b2a128585fa8a93fa4e
Rafael JPD <rafael.pezzuto@gmail.com>
2021-10-04 20:27:06 -0300

    adiciona método para criar dataset

--- scielo_dltools/utils/gbq.py
+++ scielo_dltools/utils/gbq.py
@@ -16,3 +16,16 @@ def _generate_table_id(project_name, dataset_name, table_id):
     return '{}.{}.{}'.format(project_name, dataset_name, table_id)
 
 
+def create_dataset(dataset_name):
+    bq_client = _connect()
+    dataset_id = _generate_dataset_id(bq_client.project, dataset_name)
+
+    try:
+        ds = bq_client.create_dataset(dataset_id)
+    except Forbidden:
+        print('Access denied')
+    except Conflict:
+        print('Dataset {} already exists'.format(dataset_id))
+    else:
+        print('Dataset {} created with success'.format(dataset_id))
+        return ds
