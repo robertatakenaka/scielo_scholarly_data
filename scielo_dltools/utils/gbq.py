4b4aeb5d21440e147c358628c2ea6dc3cfa936bc
Rafael JPD <rafael.pezzuto@gmail.com>
2021-10-04 20:27:47 -0300

    adiciona método para transferir tabela de Storage para BigQuery

--- scielo_dltools/utils/gbq.py
+++ scielo_dltools/utils/gbq.py
@@ -29,3 +29,36 @@ def create_dataset(dataset_name):
     else:
         print('Dataset {} created with success'.format(dataset_id))
         return ds
+def load_table_from_uri(uri, dataset_name, table_name):
+    bq_client = _connect()
+
+    table_id = _generate_table_id(
+        bq_client.project,
+        dataset_name,
+        table_name
+    )
+
+    job_config = bigquery.LoadJobConfig(
+        source_format=bigquery.SourceFormat.CSV,
+        skip_leading_rows=1,
+        autodetect=True
+    )
+
+    print('Loading table...')
+    job = bq_client.load_table_from_uri(
+        [uri],
+        table_id,
+        job_config=job_config
+    )
+
+    job.result()
+
+    table = bq_client.get_table(table_id)
+    print(
+        'Loaded {} rows and {} columns to {}'.format(
+            table.num_rows,
+            len(table.schema),
+            table_id
+        )
+    )
+    return table
