ea3c96a9fb5475fd0bd310dc9622c414c25aca3b
Rafael JPD <rafael.pezzuto@gmail.com>
2021-10-05 18:09:53 -0300

    move utils/gbq.py para libs/gbigquery.py

--- scielo_dltools/libs/gbigquery.py
+++ scielo_dltools/libs/gbigquery.py
@@ -1,5 +1,3 @@
-import argparse
-
 from google.cloud import bigquery
 from google.api_core.exceptions import Conflict, Forbidden
 
@@ -64,52 +62,3 @@ def load_table_from_uri(uri, dataset_name, table_name):
         )
     )
     return table
-
-
-def main():
-    parser = argparse.ArgumentParser(description='Google Cloud BigQuery tools')
-    subparsers = parser.add_subparsers(title='Command', metavar='', dest='command')
-
-    sp_create_dataset = subparsers.add_parser(
-        'create_dataset',
-        help=('Create dataset')
-    )
-    sp_create_dataset.add_argument(
-        'dataset_name'
-    )
-
-    sp_load_table_from_uri = subparsers.add_parser(
-        'load_table_from_uri',
-        help=("Load table from Google Cloud Storage URI")
-    )
-    sp_load_table_from_uri.add_argument(
-        'uri',
-        help='object uri'
-    )
-    sp_load_table_from_uri.add_argument(
-        'dataset',
-        help=('Dataset name')
-    )
-    sp_load_table_from_uri.add_argument(
-        'table',
-        help=('Table name')
-    )
-
-    args = parser.parse_args()
-
-    if args.command == 'create_dataset':
-        create_dataset(
-            args.dataset_name
-        )
-    elif args.command == 'load_table_from_uri':
-        load_table_from_uri(
-            args.uri,
-            args.dataset,
-            args.table
-        )
-    else:
-        parser.print_help()
-
-
-if __name__ == '__main__':
-    main()
