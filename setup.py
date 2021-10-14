87fbd03748acc236ef4f8d5fc0ea16ec9f198b49
Rafael J P D <rafael.pezzuto@gmail.com>
2021-10-14 14:50:00 -0300

    Adiciona arquivo setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='scielo-dltools',
+    version='0.1',
+    description="The SciELO Data Lake Tools",
+    author="SciELO",
+    author_email="scielo-dev@googlegroups.com",
+    license="BSD",
+    url="https://github.com/scieloorg/scielo_scholarly_data",
+    keywords='data lake',
+    maintainer_email='rafael.pezzuto@gmail.com',
+    packages=find_packages(),
+)
