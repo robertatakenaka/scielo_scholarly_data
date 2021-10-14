a871d05b8ffd2da9ea634f2e81f0585d79c131c1
Rafael J P D <rafael.pezzuto@gmail.com>
2021-10-14 15:18:41 -0300

    adiciona deps em setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from setuptools import setup, find_packages
 
 
+install_requirements=[
+    'google-cloud-bigquery',
+    'google-cloud-storage',
+]
+
 setup(
     name='scielo-dltools',
     version='0.1',
@@ -8,6 +13,7 @@ setup(
     author="SciELO",
     author_email="scielo-dev@googlegroups.com",
     license="BSD",
+    install_requires=install_requirements,
     url="https://github.com/scieloorg/scielo_scholarly_data",
     keywords='data lake',
     maintainer_email='rafael.pezzuto@gmail.com',
