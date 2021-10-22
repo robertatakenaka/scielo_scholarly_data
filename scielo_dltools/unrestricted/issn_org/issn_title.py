512a12435cba316e93149a6e0dc7392a862adc58
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2021-10-21 21:23:56 -0300

    Refatora issn_title

--- scielo_dltools/unrestricted/issn_org/issn_title.py
+++ scielo_dltools/unrestricted/issn_org/issn_title.py
@@ -12,42 +12,71 @@ def clear(value):
         return ''
 
 
-gstorage.download_file(
-    'scielo-datalake-raw',
-    'portal-issn/issns-scl_portal-issn_20210930.jsonl',
-    'issn_journals_title.jsonl'
-)
+def read_json(arq, attribs_to_read, table):
+    registros = []
+    for line in open(arq, 'r'):
+        registros.append(json.loads(line))
+    for row in registros:
+        rows = []
+        for item in attribs_to_read[:2]:
+            rows.append(clear(row.get(item)))
+        for item in attribs_to_read[2:]:
+            rows.append(clear(row.get(item)))
+            rows.append(standardizer.journal_title_for_visualization(clear(row.get(item))))
+            rows.append(standardizer.journal_title_for_deduplication(clear(row.get(item))))
+        table.append(rows)
 
-issn_keys = ['issn', 'issn_l']
-title_keys = ['key_title', 'title_proper']
-vis_keys = ['key_title_for_vis', 'title_proper_for_vis']
-dep_keys = ['key_title_for_dep', 'title_proper_for_dep']
 
-registros = []
-for line in open('issn_journals_title.jsonl', 'r'):
-    registros.append(json.loads(line))
+def write_table(arq, cols_to_write, table):
+    with open(arq, 'w') as out:
+        writer = csv.writer(out, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(cols_to_write)
+        for rows in table:
+            writer.writerow(rows)
 
-data = []
-for row in registros:
-    rows = []
-    for item in issn_keys:
-        rows.append(clear(row.get(item)))
-    for item in title_keys:
-        rows.append(clear(row.get(item)))
-    for item in title_keys:
-        rows.append(standardizer.journal_title_for_visualization(clear(row.get(item))))
-    for item in title_keys:
-        rows.append(standardizer.journal_title_for_deduplication(clear(row.get(item))))
-    data.append(rows)
 
-with open('issn_journals_title.csv', 'w') as doaj_out:
-    writer = csv.writer(doaj_out, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    writer.writerow(issn_keys + title_keys + vis_keys + dep_keys)
-    for rows in data:
-        writer.writerow(rows)
+def main():
+    BUCKET_NAME_DOWNLOAD = 'scielo-datalake-raw'
+    SOURCE_FILE_DOWNLOAD = 'index/portal-issn/journal-metadata-scl.jsonl'
+    DESTINATION_FILE_DOWNLOAD = '/home/luciano/nuvem/Dropbox/scielo/data_lake/journal-metadata-scl.jsonl'
+    BUCKET_NAME_UPLOAD = 'scielo-datalake-standardized'
+    SOURCE_FILE_UPLOAD = '/home/luciano/nuvem/Dropbox/scielo/data_lake/journal-metadata-scl.csv'
+    DESTINATION_FILE_UPLOAD = 'index/issn/journal-titles.csv'
 
-gstorage.upload_file(
-    'scielo-datalake-standardized',
-    'issn_journals_title.csv',
-    'portal-issn/issns-scl_portal-issn_20210930/title.csv'
-)
+    gstorage.download_file(
+        BUCKET_NAME_DOWNLOAD,
+        SOURCE_FILE_DOWNLOAD,
+        DESTINATION_FILE_DOWNLOAD
+    )
+
+    attribs_to_read = [
+        'issn',
+        'issn_l',
+        'key_title',
+        'title_proper'
+    ]
+
+    cols_to_write = [
+        'issn',
+        'issn_l',
+        'key_title',
+        'key_title vis',
+        'key_title dep',
+        'title_proper',
+        'title_proper vis',
+        'title_proper dep']
+
+    table = []
+
+    read_json(DESTINATION_FILE_DOWNLOAD, attribs_to_read, table)
+    write_table(SOURCE_FILE_UPLOAD, cols_to_write, table)
+
+    gstorage.upload_file(
+        BUCKET_NAME_UPLOAD,
+        SOURCE_FILE_UPLOAD,
+        DESTINATION_FILE_UPLOAD
+    )
+
+
+if __name__ == '__main__':
+    main()
