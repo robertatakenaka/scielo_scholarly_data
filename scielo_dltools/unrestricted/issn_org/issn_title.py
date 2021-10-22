1dbea915714f1c55d5edc7d939a9bee46e4e271b
Rossi-Luciano <luciano.rossi.lucross@gmail.com>
2021-10-22 13:35:46 -0300

    Adiciona argparse

--- scielo_dltools/unrestricted/issn_org/issn_title.py
+++ scielo_dltools/unrestricted/issn_org/issn_title.py
@@ -1,3 +1,4 @@
+import argparse
 import csv
 import json
 import scielo_scholarly_data.standardizer as standardizer
@@ -12,7 +13,8 @@ def clear(value):
         return ''
 
 
-def read_json(arq, attribs_to_read, table):
+def read_json(arq, attribs_to_read):
+    table = []
     registros = []
     for line in open(arq, 'r'):
         registros.append(json.loads(line))
@@ -25,23 +27,39 @@ def read_json(arq, attribs_to_read, table):
             rows.append(standardizer.journal_title_for_visualization(clear(row.get(item))))
             rows.append(standardizer.journal_title_for_deduplication(clear(row.get(item))))
         table.append(rows)
+    return table
 
 
 def write_table(arq, cols_to_write, table):
     with open(arq, 'w') as out:
         writer = csv.writer(out, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         writer.writerow(cols_to_write)
-        for rows in table:
-            writer.writerow(rows)
+        for cols in table:
+            writer.writerow(cols)
 
 
 def main():
-    BUCKET_NAME_DOWNLOAD = 'scielo-datalake-raw'
-    SOURCE_FILE_DOWNLOAD = 'index/portal-issn/journal-metadata-scl.jsonl'
-    DESTINATION_FILE_DOWNLOAD = '/home/luciano/nuvem/Dropbox/scielo/data_lake/journal-metadata-scl.jsonl'
-    BUCKET_NAME_UPLOAD = 'scielo-datalake-standardized'
-    SOURCE_FILE_UPLOAD = '/home/luciano/nuvem/Dropbox/scielo/data_lake/journal-metadata-scl.csv'
-    DESTINATION_FILE_UPLOAD = 'index/issn/journal-titles.csv'
+    parser = argparse.ArgumentParser(description='Create a standardized titles file from data in Data Lake.')
+    parser.add_argument('-bnd', action='store', dest='bucket_name_download',
+                        required=True, help='Data Lake bucket to download.')
+    parser.add_argument('-sfd', action='store', dest='source_file_download',
+                        required=True, help='Source file to download.')
+    parser.add_argument('-dfd', action='store', dest='destination_file_download',
+                        required=True, help='Destination file to download.')
+    parser.add_argument('-bnu', action='store', dest='bucket_name_upload',
+                        required=True, help='Data Lake bucket to upload.')
+    parser.add_argument('-sfu', action='store', dest='source_file_upload',
+                        required=True, help='Source file to upload.')
+    parser.add_argument('-dfu', action='store', dest='destination_file_upload',
+                        required=True, help='Destination file to upload.')
+    arguments = parser.parse_args()
+
+    BUCKET_NAME_DOWNLOAD = arguments.bucket_name_download
+    SOURCE_FILE_DOWNLOAD = arguments.source_file_download
+    DESTINATION_FILE_DOWNLOAD = arguments.destination_file_download
+    BUCKET_NAME_UPLOAD = arguments.bucket_name_upload
+    SOURCE_FILE_UPLOAD = arguments.source_file_upload
+    DESTINATION_FILE_UPLOAD = arguments.destination_file_upload
 
     gstorage.download_file(
         BUCKET_NAME_DOWNLOAD,
@@ -66,9 +84,7 @@ def main():
         'title_proper vis',
         'title_proper dep']
 
-    table = []
-
-    read_json(DESTINATION_FILE_DOWNLOAD, attribs_to_read, table)
+    table = read_json(DESTINATION_FILE_DOWNLOAD, attribs_to_read)
     write_table(SOURCE_FILE_UPLOAD, cols_to_write, table)
 
     gstorage.upload_file(
